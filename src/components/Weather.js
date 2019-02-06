cc7e6d4306b01581046be0583d8592e7580220cd
outipaloahde <outipaloahde@gmail.com>
2019-02-06 08:09:31 +0200

    bootstrap setup

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -4,7 +4,12 @@ const Weather = props => (
 <div>
     {props.temp && props.desc && <p>{props.temp} °C {props.desc}</p> }
     {props.img && <img scr={`http://openweathermap.org/img/w/${props.img}.png`} alt='weather_image'/>}
-    {props.city && props.country && <p> {props.city}, {props.country}</p>}
+    {props.city && props.country &&
+    <p> {props.city}, {props.country}
+        <button type="button" class="btn add" aria-label="Left Align">
+            <span class="glyphicon glyphicon-plus" aria-hidden="true"></span>
+        </button>
+    </p>}
     {props.error && <p>{props.error}</p>}
 </div>
 );
